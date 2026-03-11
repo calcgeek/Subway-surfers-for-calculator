a759f60011d4c2935072244fcfb41551beb1e9db
calcgeek <faigeneuman1@gmail.com>
2026-03-11 14:22:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,43 +4,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ti/getcsc.h>
+#include "gfx/gfx.h"
 #include "gfx/hiiii.h"
 #include "gfx/subwaysurf.h"
-
-
 
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 
+
+
+
+
 void drawBackground(void) {
-    gfx_SetColor(0);
-    gfx_FillScreen(0);
-// I think this is were we should put the jake pic//
-
-    // Bright neon sky gradient
-    for (int y = 0; y < 100; y++) {
-        int color = 128 + (y / 12);
-        gfx_SetColor(color);
-        gfx_HorizLine(0, y, SCREEN_WIDTH);
-    }
-
-    // Dark ground
-    gfx_SetColor(16);
-    gfx_FillRectangle(0, 100, SCREEN_WIDTH, 140);
+    gfx_SetColor(31);
+    gfx_FillScreen(255);
+gfx_FillRectangle(0,0,SCREEN_WIDTH,12);
+gfx_SetColor(63);
+gfx_FillRectangle(0,12,SCREEN_WIDTH,12);
+gfx_SetColor(95);
+gfx_FillRectangle(0,24,SCREEN_WIDTH,12);
+gfx_SetColor(127);
+gfx_FillRectangle(0,36,SCREEN_WIDTH,12);
+gfx_SetColor(159);
+gfx_FillRectangle(0,48,SCREEN_WIDTH,12);
+gfx_SetColor(191);
+gfx_FillRectangle(0,60,SCREEN_WIDTH,12);
+gfx_SetColor(223);
+gfx_FillRectangle(0,72,SCREEN_WIDTH,12);
 }
-
-
 
 void drawTitle(void) {
     gfx_SetTextScale(3, 3);
     gfx_SetColor(206);
-    gfx_PrintStringXY("SUBWAY", 90, 15);
+    gfx_PrintStringXY("SUBWAY", 90, 1);
     gfx_SetColor(141);
-    gfx_PrintStringXY("SURFERS", 85, 35);
+    gfx_PrintStringXY("SURFERS", 85, 23);
 
-    gfx_SetTextScale(1, 1);
+    gfx_SetTextScale(2, 1);
     gfx_SetColor(226);
-    gfx_PrintStringXY(">> CE <<", 135, 55);
+    gfx_PrintStringXY(">> CE <<", 117, 50);
 }
 
 void drawMenuButton(int x, int y, int width, int height, const char *text, int selected) {
@@ -64,13 +67,6 @@ void drawMenuButton(int x, int y, int width, int height, const char *text, int s
     gfx_PrintStringXY(text, x + 10, y + 8);
 }
 
-void drawMenuBackground(void) {
-    // Semi-transparent dark overlay - lighter so train shows through
-    gfx_SetColor(40);
-    for (int i = 0; i < 3; i++) {
-        gfx_FillRectangle(0, 130 + (i * 35), SCREEN_WIDTH, 28);
-    }
-}
 
 int main(void) {
     gfx_Begin();
@@ -80,44 +76,41 @@ int main(void) {
     int numOptions = 3;
     //uint8_t prevKeys = 0;//
 
-    while (1) {
+   // while (1)// {
         drawBackground();
+         gfx_SetPalette(global_palette, sizeof_global_palette, 0);
+        //bottom right
+      gfx_ScaledTransparentSprite_NoClip(tree, 240, 150, 2,2);
+        //botton left tree
+      gfx_ScaledTransparentSprite_NoClip(tree, -10, 150, 2,2);
 
-        drawTitle();
-        drawMenuBackground();
+      gfx_ScaledTransparentSprite_NoClip(hiiii, -13, 0, 5, 5);
 
-        const char *options[] = {"PLAY GAME", "SETTINGS", "EXIT"};
+    drawTitle();
+/*gfx_SetTransparentColor(whatever index your color white is);
+    gfx_TransparentSprite(sprite, x, y);*/
+
+        const char *options[] = {"PLAY GAME", "MORE OPTIONS", "EXIT"};
 
         for (int i = 0; i < numOptions; i++) {
-            int y = 140 + (i * 35);
+            int y = 130 + (i * 35);
             drawMenuButton(70, y, 180, 28, options[i], (i == selected));
         }
 
         gfx_SetTextScale(1, 1);
         gfx_SetColor(206);
-        gfx_PrintStringXY("UP/DOWN | CLEAR exit", 100, 235);
+        gfx_PrintStringXY("UP/DOWN | CLEAR exit", 100, 230);
+ ;
 
         gfx_SwapDraw();
 
-        kb_Scan();
-        if(kb_IsDown(kb_KeyUp)) {
 
-        if (kb_IsDown(kb_KeyUp)) while(kb_AnyKey())
-            if (selected < 0) selected = numOptions - 1;
-        }
 
-        if(kb_IsDown(kb_KeyUp)) {
-            selected++;
-            if (selected >= numOptions) selected = 0;
-        }
+   /* Waits for a key */
+    while(!os_GetCSC()){};
 
-        if (kb_IsDown(kb_Clear)) while(kb_AnyKey()) {
-            break;
-        }
-
-        while(kb_AnyKey());
-    }
-
+    /* End graphics drawing */
     gfx_End();
-    return 0;
+
+    return 0 ;
 }
